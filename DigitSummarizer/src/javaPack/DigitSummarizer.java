6d7c95b94dc4c8423e9e7d10f6d1009a0638ed26
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2026-05-14 11:21:09 -0400

    Add files via upload

---
+++ DigitSummarizer/src/javaPack/DigitSummarizer.java
@@ -0,0 +1,29 @@
+package javaPack;
+
+public class DigitSummarizer 
+{
+	public static int main()
+	{
+		int sum = 0;
+		int n = 0;
+	    int one = 9;
+	    int two = 5;
+	    
+		while( n > 0);
+		{
+			sum =  one +  two;
+		}
+			 return sum;
+	}
+		
+
+	public static int digitalRoot(int sum) 
+	{
+		int x = sum;
+		while(x >= 10)
+		{
+			main();
+	    }
+		return sum;
+	}
+}
---
+++ DigitSummarizer/src/javaPack/Main.java
@@ -0,0 +1,11 @@
+package javaPack;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int num = 95;
+        int root = DigitSummarizer.digitalRoot(num);
+        System.out.println("digital root = " + root);
+    }
+}
